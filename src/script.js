7c6aa7eac927a48c7daffb96ec6a621b1226299a
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-06-02 18:48:01 -0400

    opening created
    typerwriter created and headshot

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+let toggleButton = document.querySelector('#toggle-button');
+let body = document.querySelector('body');
+toggleButton.addEventListener('click', function() {
+  body.classList.toggle('dark-theme');
+});
 
+new Typewriter('#badge-name', {
+  strings: ('you can call me Nic'),
+  autoStart: true,
+  loop: false,
+  cursorClassName: 'Typewriter_cursor',
+  
+});
