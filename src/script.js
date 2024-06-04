532d7197a6cc00d67c1cec5c959a955352f9fdb6
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-06-03 21:25:36 -0400

    about me section started
    JS done for animation and typewriter

--- src/script.js
+++ src/script.js
@@ -5,9 +5,30 @@ toggleButton.addEventListener('click', function() {
 });
 
 new Typewriter('#badge-name', {
-  strings: ('you can call me Nic'),
+  strings: ('you can call me <strong>Nic</strong>'),
   autoStart: true,
   loop: false,
   cursorClassName: 'Typewriter_cursor',
-  
+
 });
+
+
+        let text = document.getElementById('about-me');
+        let newDom = '';
+        let animationDelay = 1*100;
+
+        for(let i = 0; i < text.innerText.length; i++)
+        {
+            newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+        }
+
+        text.innerHTML = newDom;
+        let length = text.children.length;
+
+        for(let i = 0; i < length; i++)
+        {
+
+            text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+
+
+        }
